d3296d3fac7e0209bd85fd6147aa45a236098a2a
binhhothanhsts <141118946+binhhothanhsts@users.noreply.github.com>
2023-12-22 15:35:46 +0700

    -- Add SDK datatrans for android

--- android/src/main/kotlin/com/sts/datatrans_plugin_flutter/DatatransPluginFlutterPlugin.kt
+++ android/src/main/kotlin/com/sts/datatrans_plugin_flutter/DatatransPluginFlutterPlugin.kt
@@ -1,35 +1,112 @@
 package com.sts.datatrans_plugin_flutter
 
-import androidx.annotation.NonNull
-
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import ch.datatrans.payment.api.Transaction
+import ch.datatrans.payment.api.TransactionListener
+import ch.datatrans.payment.api.TransactionRegistry
+import ch.datatrans.payment.api.TransactionSuccess
+import ch.datatrans.payment.exception.TransactionException
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.plugin.common.PluginRegistry.ActivityResultListener
+import io.flutter.plugin.common.PluginRegistry.NewIntentListener
+import io.flutter.plugin.common.PluginRegistry.RequestPermissionsResultListener
 
 /** DatatransPluginFlutterPlugin */
-class DatatransPluginFlutterPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+class DatatransPluginFlutterPlugin : MethodCallHandler,
+    NewIntentListener,
+    RequestPermissionsResultListener,
+    ActivityResultListener,
+    FlutterPlugin,
+    ActivityAware {
 
-  override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "datatrans_plugin_flutter")
-    channel.setMethodCallHandler(this)
-  }
-
-  override fun onMethodCall(call: MethodCall, result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+    companion object {
+        const val TOKEN_MOBILE = "d065e4d4261c6f4c9fac7b92c373976c0335774464a25674";
     }
-  }
 
-  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+    private lateinit var context:Context
+    private lateinit var channel: MethodChannel
+    private val transaction = Transaction(TOKEN_MOBILE)
+
+
+    init {
+        transaction.apply {
+            options.appCallbackScheme = "com.sts.datatrans_plugin_flutter_example"
+            listener = object :TransactionListener {
+                override fun onTransactionError(exception: TransactionException) {
+                   exception.printStackTrace()
+                }
+
+                override fun onTransactionSuccess(result: TransactionSuccess) {
+                   result.transactionId
+                }
+            }
+            options.isTesting = true
+            options.useCertificatePinning = false
+            options.suppressCriticalErrorDialog = true
+        }
+    }
+
+
+    override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "datatrans_plugin_flutter")
+        channel.setMethodCallHandler(this)
+    }
+
+    override fun onMethodCall(call: MethodCall, result: Result) {
+        if (call.method == "getPlatformVersion") {
+            result.success("Android ${android.os.Build.VERSION.RELEASE}")
+        } else {
+            result.notImplemented()
+        }
+    }
+
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
+
+    override fun onNewIntent(intent: Intent): Boolean {
+      Log.e("activity", "onNewIntent")
+      return true
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray,
+    ): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?,
+    ): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+      context = binding.activity
+      TransactionRegistry.startTransaction(binding.activity, transaction)
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
+       Log.e("activity", "Detached")
+    }
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+      Log.e("activity", "Reattached")
+    }
+
+    override fun onDetachedFromActivity() {
+      Log.e("activity", "Detached")
+    }
 }
--- example/android/app/src/main/kotlin/com/sts/datatrans_plugin_flutter_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/sts/datatrans_plugin_flutter_example/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.sts.datatrans_plugin_flutter_example
 
+import android.os.Bundle
+import com.sts.datatrans_plugin_flutter.DatatransPluginFlutterPlugin
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
